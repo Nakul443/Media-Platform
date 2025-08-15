7bf5d8ce71734923defafcce5e832741f9dec78b
Nakul <nakul.kumar627@gmail.com>
2025-08-15 16:56:51 +0000

    POST API implemented and tested

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,23 @@ db.connect((err) => {
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from TypeScript Express!');
 });
+
+
+app.post('/media/:id/view', (req: Request, res: Response) => {
+    const IP = req.body.IP; // will be sent through the request body
+    const mediaId = req.params.id; // will be sent through the URL parameter or the frontend or postman
+    if (!IP || !mediaId) {
+        return res.status(400).json({ error: 'IP and media ID are required' });
+    }
+
+    const sql = 'INSERT INTO media_view_log (media_id,viewed_by_ip) values (?,?)';
+
+    db.query(sql, [mediaId,IP], (err, result) => {
+        if (err) {
+            console.error("Error inserting view log:", err.message);
+            return res.status(500).json({ error: 'Database error' });
+        }
+        console.log(`Media ID ${mediaId} viewed by IP ${IP}`);
+        res.status(200).json({ message: 'View logged successfully' });
+    });
+});
