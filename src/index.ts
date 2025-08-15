a21c6b7af36febcfebe46c9523dcbce4b2d1a0d5
Nakul <nakul.kumar627@gmail.com>
2025-08-15 14:30:26 +0000

    Setup express

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// src/index.ts
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from TypeScript Express!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
